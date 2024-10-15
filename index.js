47709a57d57ccb70295ce443968e08bfd193fd35
Blaise7 <blaisearnaud9@gmail.com>
2024-10-15 21:48:26 +0200

    added content on index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello, World!');
+
+let name = "Blaise";
+let age = 35;
+let favoriteColor = "blue";
+
+console.log(`My name is ${name}, I am ${age} years old, and my favorite color is ${favoriteColor}.`);
+
