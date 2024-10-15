ad08ee2c9710e488ae2a55b8c0ac50b02a2d1741
Blaise7 <blaisearnaud9@gmail.com>
2024-10-15 21:56:17 +0200

    added content on index.js

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ let favoriteColor = "blue";
 
 console.log(`My name is ${name}, I am ${age} years old, and my favorite color is ${favoriteColor}.`);
 
+let number1 = 15;
+let number2 = 3;
+
+console.log("Addition: " + (number1 + number2));
+console.log("Subtraction: " + (number1 - number2));
+console.log("Multiplication: " + (number1 * number2));
+console.log("Division: " + (number1 / number2));
